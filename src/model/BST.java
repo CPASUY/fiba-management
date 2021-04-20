c885c91113ca2ddb56dd54f672ed144bb24ea916
anderson-154 <54720103+anderson-154@users.noreply.github.com>
2021-04-20 17:07:01 -0500

    add classes and generics

---
+++ src/model/BST.java
@@ -0,0 +1,59 @@
+package model;
+
+public class BST<K extends Comparable<K>, V> implements IBST<K,V>{
+
+	@Override
+	public void insertE(K key, V value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean removeE(K key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Node<K, V> getRoot() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V searchE(K key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String inOrden() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String postOrden() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String preOrden() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/model/IAVL.java
@@ -0,0 +1,10 @@
+package model;
+
+public interface IAVL<K extends Comparable<K>,V> extends IBST<K, V> {
+
+	public void insertE(K key, V value);
+	public boolean removeE(K key);
+	public void rebalance();
+	public void leftRotate(Node<K,V> node);
+	public void rightRotate(Node<K,V> node);
+}
