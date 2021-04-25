edd2fb988fa2430a990659031b77758d8838c9ae
CPASUY <58858439+CPASUY@users.noreply.github.com>
2021-04-25 11:36:17 -0500

    Fix problems in the ABB class

--- src/model/IBST.java
+++ src/model/IBST.java
@@ -1,10 +1,6 @@
 package model;
 
-<<<<<<< HEAD
 public interface IBST<K extends Comparable<K>,V extends Comparable <V>> {
-=======
-public interface IBST<K extends Comparable <K>,V extends Comparable <V>>{
->>>>>>> d90118be5ddbce5f1954e57ee9687429d6e99a9f
 
 	public void insertE(K key, V value);
 	public void insertE(Node<K,V>n1,Node<K,V>n2);
